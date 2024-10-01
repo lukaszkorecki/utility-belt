d490bae5a1c5b353ef543d27ef9252ced6fe09d4
Łukasz Korecki <lukasz@korecki.me>
2024-10-01 11:56:52 -0700

    Fix nrepl component to be idempotent

--- src/utility_belt/component/nrepl.clj
+++ src/utility_belt/component/nrepl.clj
@@ -2,13 +2,17 @@
   (:require [utility-belt.component :as component]
             nrepl.server))
 
-(defn create [{:keys [host port]
-               :or {host "0.0.0.0"}
-               :as config}]
-
+(defn create
+  "Creates an nREPL server component, by default it binds to loopback address"
+  [{:keys [host port]
+    :or {host "0.0.0.0"}
+    :as config}]
+  {:pre [(pos? port)]}
   (component/map->component {:init-val config
-                             :start #(assoc % :server (nrepl.server/start-server :port port
-                                                                                 :host host))
+                             :start (fn [this]
+                                      (if (:server this)
+                                        this
+                                        (assoc this :server (nrepl.server/start-server :port port :bind host))))
 
                              :stop (fn [this]
                                      (if-let [server (:server this)]

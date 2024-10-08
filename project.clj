109b7f5165d6cd8794b205a99fe1101633075457
Łukasz Korecki <lukasz@korecki.me>
2024-10-08 11:19:11 -0700

    Fix snapshot release for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/utility-belt "2.0.0-SNAPSHOT-3"
+(defproject org.clojars.lukaszkorecki/utility-belt "2.0.0-SNAPSHOT"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :license "MIT"
   :url "https://github.com/lukaszkorecki/utility-belt"
@@ -9,9 +9,7 @@
   :dependencies [[org.clojure/clojure "1.12.0"]]
 
   :global-vars {*warn-on-reflection* true}
-
   :profiles {:dev
-             {:dependencies [[cheshire "5.13.0"]
-                             [com.stuartsierra/component "1.1.0"]
+             {:dependencies [[com.stuartsierra/component "1.1.0"]
                              [org.clojure/tools.logging "1.3.0"]
                              [ch.qos.logback/logback-classic "1.5.8"]]}})

aa1b9c1b40549829b6ae1a5d89e98be23f91b26d
Łukasz Korecki <lukasz@korecki.me>
2024-10-30 12:23:28 -0700

    relese 2.0.1-SNAPSHOT - fix dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/utility-belt "2.0.0-SNAPSHOT"
+(defproject org.clojars.lukaszkorecki/utility-belt "2.0.1-SNAPSHOT"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :license "MIT"
   :url "https://github.com/lukaszkorecki/utility-belt"
@@ -6,10 +6,10 @@
                                    :username :env/clojars_username
                                    :password :env/clojars_password}}
 
-  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [com.stuartsierra/component "1.1.0"]]
 
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev
-             {:dependencies [[com.stuartsierra/component "1.1.0"]
-                             [org.clojure/tools.logging "1.3.0"]
+             {:dependencies [[org.clojure/tools.logging "1.3.0"]
                              [ch.qos.logback/logback-classic "1.5.8"]]}})

e21a4f3fd26503c05c35dad633a6fa37aa7abf9e
Łukasz Korecki <lukasz@korecki.me>
2025-04-23 13:56:21 -0700

    Use clojure.tools.logging rather than print* for messages

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/utility-belt "2.1.0"
+(defproject org.clojars.lukaszkorecki/utility-belt "2.1.1"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :license "MIT"
   :url "https://github.com/lukaszkorecki/utility-belt"
@@ -8,9 +8,9 @@
 
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [com.stuartsierra/component "1.1.0"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [cheshire "5.13.0"]]
 
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev
-             {:dependencies [[org.clojure/tools.logging "1.3.0"]
-                             [ch.qos.logback/logback-classic "1.5.8"]]}})
+             {:dependencies [[ch.qos.logback/logback-classic "1.5.8"]]}})

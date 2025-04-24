ec54c611e5a946804fb29f31d1c4256ef16df1e7
Łukasz Korecki <lukasz@korecki.me>
2025-04-24 11:16:52 -0700

    v2.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.lukaszkorecki/utility-belt "2.1.1"
+(defproject org.clojars.lukaszkorecki/utility-belt "2.2.0"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :license "MIT"
   :url "https://github.com/lukaszkorecki/utility-belt"

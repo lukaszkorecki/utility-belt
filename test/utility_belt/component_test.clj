a9b281e2b8fe11296bacaa04da2b01f730c20db3
Łukasz Korecki <lukasz@korecki.me>
2025-09-02 12:31:34 -0700

    Component improvements

--- test/utility_belt/component_test.clj
+++ test/utility_belt/component_test.clj
@@ -13,18 +13,22 @@
           :d :d}
          (component.util/deps [:a :b {:c :f} :d]))))
 
-(defrecord Fake
-  [dep-a dep-b dep-d]
+(defrecord Fake [dep-a dep-b dep-d]
   component/Lifecycle
   (start [this] this)
   (stop [this] this))
+
+(defprotocol ISomething
+  :extend-via-metadata true
+  (do-something [this])
+  (do-more [this]))
 
 (deftest using+-test
   (let [com (component.util/map->system
              {:test (component.util/using+
                      (->Fake nil nil nil)
                      [:a :b {:d-alias :d}])
-               ;; fake components
+              ;; fake components
               :a (fn [] :dep-a)
               :b (fn [] :dep-b)
               :d (fn [] :dep-d-as-alias)})
@@ -35,10 +39,12 @@
 
 (deftest map-components-test
   (let [mini-sys (component.util/map->system
-                  {:test (component.util/map->component {:init {:hello :world}
-                                                         :start #(assoc % :started true)
-                                                         :stop #(assoc % :started false
-                                                                       :stopped true)})})]
+                  {:test (component/using (component.util/map->component {:init {:hello :world}
+                                                                          :start #(assoc % :started true)
+                                                                          :stop #(assoc % :started false
+                                                                                        :stopped true)})
+                                          [:dep])
+                   :dep :HELLO})]
 
     (testing "initial state"
       (is (= {:hello :world}
@@ -47,9 +53,28 @@
     (testing "starting & stopping"
       (let [sys (component/start mini-sys)]
         (is (= {:hello :world
-                :started true}
+                :started true
+                :dep :HELLO}
                (:test sys)))
         (is (= {:hello :world
                 :started false
-                :stopped true}
+                :stopped true
+                :dep :HELLO}
                (:test (component/stop sys))))))))
+
+(deftest map-components-with-extra-protocol-test
+
+  (let [thing (component/start (component.util/map->component
+                                {:init {:hello :world}
+                                 :start #(assoc % :started true)
+                                 :stop #(assoc % :started false
+                                               :stopped true)
+                                 ;; qual kws work
+                                 ::do-something (fn [this] (str "Did something with " (:hello this)))
+                                 ;; symbols work too:
+                                 'utility-belt.component-test/do-more (fn [this] (str "Did more with " (:hello this)))}))]
+    (is (= "Did something with :world"
+           (do-something thing)))
+
+    (is (= "Did more with :world"
+           (do-more thing)))))

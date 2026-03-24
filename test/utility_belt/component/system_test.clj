67a4644080f47d9df5911eceb88202c49d96efbb
Darren Kim <darren.kim@peerspace.com>
2026-03-24 14:21:43 -0400

    allow plain function for component map fn

--- test/utility_belt/component/system_test.clj
+++ test/utility_belt/component/system_test.clj
@@ -1,46 +1,68 @@
 (ns utility-belt.component.system-test
   (:require [clojure.test :refer [deftest testing is]]
             [utility-belt.component :as util.component]
-            [utility-belt.component.system :as util.system]))
+            [utility-belt.component.system :as util.system]
+            [utility-belt.lifecycle :as lifecycle]))
 
 (def counter
   (atom 0))
 
 (defn make-system []
   {:static :value
-   :thing (util.component/map->component {:start (fn [this]
-                                                   (swap! counter inc)
-                                                   (assoc this :started true :stopped false))
+   :thing (-> {:start (fn [this]
+                        (swap! counter inc)
+                        (assoc this :started true :stopped false))
+               :stop (fn [this]
+                       (swap! counter dec)
+                       (assoc this :stopped true :started false))}
+              util.component/map->component)})
 
-                                          :stop (fn [this]
-                                                  (swap! counter dec)
-                                                  (assoc this :stopped true :started false))})})
+(defn- assert-prod-behavior [component-map-fn]
+  (let [store (atom nil)]
+    (-> {:store store
+         :component-map-fn component-map-fn}
+        util.system/setup-for-production)
+    (is (some? @store))
+    (is (= :value (-> @store :static)))
+    (is (= 1 @counter))
+    (is (contains? @lifecycle/registerd-hooks :shutdown-system))
+    (lifecycle/remove-shutdown-hooks!)))
 
-(deftest setup-for-dev-test
-  (let [{:keys [start-system stop-system get-system]} (util.system/setup-for-dev {:component-map-fn 'utility-belt.component.system-test/make-system
-                                                                                  :reloadable? false})]
+(deftest setup-for-production-test
+  (testing "passing qualified symbol for component map fn"
+    (assert-prod-behavior 'utility-belt.component.system-test/make-system))
+  (testing "passing plain function for component map fn"
+    (assert-prod-behavior utility-belt.component.system-test/make-system)))
 
+(defn- assert-dev-behavior [component-map-fn]
+  (let [control (-> {:component-map-fn component-map-fn
+                     :reloadable? false}
+                    util.system/setup-for-dev)
+        {:keys [start-system stop-system get-system]} control]
     (testing "system can be started via provided fns"
       (testing "system starts only once"
         (start-system)
         (start-system)
         (is (= 1 @counter)))
-
       (testing "we can get the 'components'"
         (is (= :value (-> (get-system) :static)))))
-
     (testing "stopping"
       (stop-system)
-
       (is (nil? (-> (get-system) :static)))
-
       (testing "stop handler was called on the component"
         (is (zero? @counter))))
-
     (testing "starting again, but with extra components"
       (start-system {:another-static :another-thing})
+      (is (= 1 @counter))
       (is (= :another-thing (-> (get-system) :another-static)))
-      (stop-system))))
+      (stop-system)
+      (is (zero? @counter)))))
+
+(deftest setup-for-dev-test
+  (testing "passing qualified symbol for component map fn"
+    (assert-dev-behavior 'utility-belt.component.system-test/make-system))
+  (testing "passing plain function for component map fn"
+    (assert-dev-behavior utility-belt.component.system-test/make-system)))
 
 (deftest setup-for-test-test
   (testing "provides utility for unit tests"
